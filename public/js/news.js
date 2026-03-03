e12c46dea8e0833bc17ed712278fa54e3a296ddf
MridulSharma2552007 <mridul25sharma52007@gmail.com>
2026-03-03 22:20:32 +0530

    init commit

---
+++ public/js/news.js
@@ -0,0 +1,31 @@
+const NEWS_API_KEY = "69caa40994344ecea2037c2f38e5c85c";
+async function loadNews() {
+  try {
+    const response = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=in&apiKey=${NEWS_API_KEY}`,
+    );
+
+    const data = await response.json();
+
+    const articles = data.articles;
+
+    const container = document.getElementById("news-container");
+
+    container.innerHTML = articles
+      .map(
+        (article) => `
+      <div class="news-card">
+        <img src="${article.urlToImage || "https://via.placeholder.com/400"}" />
+        <h3>${article.title}</h3>
+        <p>${article.description || "No description available."}</p>
+        <a href="${article.url}" target="_blank">Read More →</a>
+      </div>
+    `,
+      )
+      .join("");
+  } catch (error) {
+    console.error("Error loading news:", error);
+  }
+}
+
+loadNews();
